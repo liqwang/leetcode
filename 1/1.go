d6d9f6a41f0030dd6fcbe13d27fc3507c3956d07
liqwang <liqwang2@gmail.com>
2025-05-30 01:03:36 +0800

    feat: solve #1

---
+++ 1/1.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func twoSum(nums []int, target int) []int {
+	indexes := make(map[int]int)
+	for i, num := range nums {
+		need := target - num
+		if index, found := indexes[need]; found {
+			return []int{index, i}
+		}
+		indexes[num] = i
+	}
+	return nil; // never reach
+}
