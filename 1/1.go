f8ce91ff943036c5dab69c7cc93c7a9f8ee40555
liqwang <liqwang2@gmail.com>
2025-06-02 23:25:12 +0800

    docs: add comments for solution #1

--- 1/1.go
+++ 1/1.go
@@ -1,5 +1,8 @@
+// https://leetcode.com/problems/two-sum
 package solution
 
+// Time: O(n)
+// Space: O(n)
 func twoSum(nums []int, target int) []int {
 	indexes := make(map[int]int)
 	for i, num := range nums {
