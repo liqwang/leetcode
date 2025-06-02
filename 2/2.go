f7219f73b2b7a40773e50234de0ad0edcf82fc71
liqwang <liqwang2@gmail.com>
2025-06-02 11:36:15 +0800

    feat: solve problem #2 https://leetcode.com/problems/add-two-numbers

---
+++ 2/2.go
@@ -0,0 +1,31 @@
+package solution
+
+import . "github.com/liqwang/leetcode/lib"
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    dummyHead := &ListNode{Val: 0}
+    cur := dummyHead
+    carry := 0
+    for l1 != nil || l2 != nil || carry != 0 {
+        x := 0
+        if l1 != nil {
+            x = l1.Val
+        }
+        y := 0
+        if l2 != nil {
+            y = l2.Val
+        }
+        sum := x + y + carry
+        carry = sum / 10
+        cur.Next = &ListNode{Val: sum % 10}
+
+        cur = cur.Next
+        if l1 != nil {
+            l1 = l1.Next
+        }
+        if l2 != nil {
+            l2 = l2.Next
+        }
+    }
+    return dummyHead.Next
+}
