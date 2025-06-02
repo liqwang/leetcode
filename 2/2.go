37a16f8888139cae5dceb12e18786dc5dc1faeee
liqwang <liqwang2@gmail.com>
2025-06-02 23:43:41 +0800

    refactor(#2): simplify code

--- 2/2.go
+++ 2/2.go
@@ -5,30 +5,23 @@ import . "github.com/liqwang/leetcode/lib"
 
 // Time: O(max(m,n))
 // Space: O(1), not includes the necessary cost of the returned linked list
-func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-    dummyHead := &ListNode{Val: 0}
-    cur := dummyHead
+func addTwoNumbers(l1, l2 *ListNode) *ListNode {
+    dummy := &ListNode{}
+    cur := dummy
     carry := 0
     for l1 != nil || l2 != nil || carry != 0 {
-        x := 0
+        sum := carry
         if l1 != nil {
-            x = l1.Val
-        }
-        y := 0
-        if l2 != nil {
-            y = l2.Val
-        }
-        sum := x + y + carry
-        carry = sum / 10
-        cur.Next = &ListNode{Val: sum % 10}
-
-        cur = cur.Next
-        if l1 != nil {
+            sum += l1.Val
             l1 = l1.Next
         }
         if l2 != nil {
+            sum += l2.Val
             l2 = l2.Next
         }
+        carry = sum / 10
+        cur.Next = &ListNode{Val: sum % 10}
+        cur = cur.Next
     }
-    return dummyHead.Next
+    return dummy.Next
 }
